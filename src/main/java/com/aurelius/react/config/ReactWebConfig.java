d4c0d0df1521415125bc287d64c7984f799cb7eb
Tan Kim Loong Ian <aurelius0523@gmail.com>
2018-05-10 19:37:56 +0800

    Updated React's inclusion to be more straightforward

--- src/main/java/com/aurelius/react/config/ReactWebConfig.java
+++ src/main/java/com/aurelius/react/config/ReactWebConfig.java
@@ -3,16 +3,12 @@ package com.aurelius.react.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Defines Configuration for React Frontend. React Frontend is included as
- * webjars built from spring-react-frontend project. A copy of index.html needs
- * to be created in main/resources/public which points to bundle.js in webjar to
- * serve React. Note that in the spring-react-frontend project the index.html
- * will use bundle.js directly, not from webjar.
+ * webjars built from spring-react-frontend project.
  * 
  * @author kim.loong.tan
  *
@@ -20,12 +16,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @ComponentScan(basePackages = "com.aurelius.react.config", excludeFilters = @Filter(Configuration.class))
 public class ReactWebConfig implements WebMvcConfigurer {
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		if (!registry.hasMappingForPattern("/webjars/**")) {
-			registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-		}
-	}
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
